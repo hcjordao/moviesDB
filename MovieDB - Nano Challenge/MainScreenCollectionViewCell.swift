926556331b1175523abe713d5a877aded2866b2c
athosLagemann <athoslagemann@gmail.com>
2017-06-02 17:06:43 -0300

    Created CollectionView object in the storyboard; Created CollectionViewCell class; Made basic configuration of CollectionView with provisional values.

---
+++ MovieDB - Nano Challenge/MainScreenCollectionViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MainScreenCollectionViewCell.swift
+//  MovieDB - Nano Challenge
+//
+//  Created by Athos Lagemann on 02/06/17.
+//  Copyright © 2017 nano Challenge Movie DB. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenCollectionViewCell: UICollectionViewCell {
+	
+	@IBOutlet var movieImageView: UIImageView!
+	
+	func loadDefaultImg() {
+		
+		movieImageView.backgroundColor = UIColor.gray
+		
+	}
+	
+	func loadMovieImage(image img: UIImage) {
+		
+		movieImageView.image = img
+	}
+	
+    
+}
--- MovieDB - Nano Challenge/ViewController.swift
+++ MovieDB - Nano Challenge/ViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+	@IBOutlet var mainCollectionView: UICollectionView!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+		
+		mainCollectionView.delegate = self
+		mainCollectionView.dataSource = self
+		mainCollectionView.prefetchDataSource = self
+		
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+	// MARK: - CollectionViewSettings
+	func numberOfSections(in collectionView: UICollectionView) -> Int {
+		return 1
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+		// Provisório
+		return 3
+	}
+	
+	func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		let cell = mainCollectionView.dequeueReusableCell(withReuseIdentifier: "movieCell", for: indexPath) as! MainScreenCollectionViewCell
+		
+		cell.loadDefaultImg()
+		
+		return cell
+	}
+	
 
 }
 
+// MARK: - Prefetch
+extension ViewController : UICollectionViewDataSourcePrefetching {
+	func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+		// chama a classe com metodo pra preloadar as imagens
+	}
+}
