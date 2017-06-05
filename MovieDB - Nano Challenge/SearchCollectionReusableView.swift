0b2e53848fd290971df2f145ff1b523e68e88c03
lauracorssac <lauracorssac@hotmail.com>
2017-06-05 16:32:28 -0300

    collection view of search screen

---
+++ MovieDB - Nano Challenge/SearchCollectionReusableView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchCollectionReusableView.swift
+//  MovieDB - Nano Challenge
+//
+//  Created by Laura Corssac on 05/06/17.
+//  Copyright © 2017 nano Challenge Movie DB. All rights reserved.
+//
+
+import UIKit
+
+class SearchCollectionReusableView: UICollectionReusableView {
+    
+    
+    
+    
+    
+    
+    
+        
+}
