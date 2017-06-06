6869a0195885fa116345c1120a56079089ac75c3
Douglas Gehring <douglasgehring@Douglass-MacBook-Pro.local>
2017-06-06 14:55:11 -0300

    Finishing requests manager logic and adding movie images class to the project.

--- MovieDB - Nano Challenge/Movie.swift
+++ MovieDB - Nano Challenge/Movie.swift
@@ -17,38 +17,131 @@ class Movie: AnyObject {
     let duration: Int! // depois
     let overview: String
     let posterPath: String
-    var genres: [Int]
+    var genres: [String] = []
     let rating: Float
     let id: Int
+    let movieImages:[Images]!
+    let director: String!
     
     
-    
-    init(originalTitle:String, releaseDate:String, language:String, overview:String, posterPath:String, genres:[Int], rating:Float, id:Int) {
+    init(originalTitle:String, releaseDate:String, language:String, overview:String, posterPath:String, genres:Array<NSDictionary>, rating:Float, id:Int) {
         
         self.originalTitle = originalTitle
         self.releaseDate = releaseDate
         self.language = language
         self.overview = overview
         self.posterPath = posterPath
-        self.genres = genres
         self.rating = rating
         self.id = id
         self.castList = nil
         self.duration = nil
+        self.movieImages = nil
+        self.director = nil
+        self.genres = self.setMovieGendersById(genders: genres)
+
+        
     }
     
-    init(originalTitle:String, releaseDate:String, language:String, overview:String, posterPath:String, genres:[Int], rating:Float, id:Int, duration:Int, castList:[Actors]) {
+    init(originalTitle:String, releaseDate:String, language:String, overview:String, posterPath:String, genres:Array<NSDictionary>, rating:Float, id:Int, duration:Int, castList:[Actors], movieImages:[Images], director:String) {
         
         self.originalTitle = originalTitle
         self.releaseDate = releaseDate
         self.language = language
         self.overview = overview
         self.posterPath = posterPath
-        self.genres = genres
         self.rating = rating
         self.id = id
         self.castList = castList
         self.duration = duration
+        self.movieImages = movieImages
+        self.director = director
+        self.genres = self.setMovieGendersById(genders: genres)
+       
+        
+        
+    }
+    
+    
+    private func setMovieGendersById(genders:Array<NSDictionary>)->[String]{
+        
+        
+        var genderArray:[String] = []
+        
+        for gender in genders{
+            
+             genderArray.append(self.defineGenderById(id: gender["id"] as! Int))
+        }
+        
+        
+        return genderArray
+    }
+    
+    
+    private func defineGenderById(id:Int)->String{
+        
+        
+        switch id {
+        case 28:
+            return "Action"
+            
+        case 12:
+            return "Adventure"
+            
+        case 16:
+            return "Animation"
+            
+        case 35:
+            return "Comedy"
+            
+        case 80:
+            return "Crime"
+            
+        case 99:
+            return "Documentary"
+        case 18:
+            return "Drama"
+            
+        case 10751:
+            return "Family"
+            
+        case 14:
+            return "Fantasy"
+            
+        case 36:
+            return "History"
+            
+        case 27:
+            return "Horror"
+            
+        case 10402:
+            return "Music"
+            
+        case 9648:
+            return "Mystery"
+            
+        case 10749:
+            return "Romance"
+            
+        case 878:
+            return"Science Fiction"
+            
+        case 10770:
+            return "TV Movie"
+            
+        case 53:
+            return "Thriller"
+            
+        case 10752:
+            return "War"
+            
+        case 37:
+            return "Western"
+            
+        default:
+            return "None"
+        }
+        
+        
         
     }
     
