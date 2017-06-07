452d9ae1e9d4fb1a5a08176b44de639068cc6048
athosLagemann <athoslagemann@gmail.com>
2017-06-07 14:01:39 -0300

    Preparing for merge

---
+++ MovieDB - Nano Challenge/MainScreenCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MainScreenCollectionViewCell.swift
+//  MovieDB - Nano Challenge
+//
+//  Created by Athos Lagemann on 07/06/17.
+//  Copyright © 2017 nano Challenge Movie DB. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ MovieDB - Nano Challenge/TransitionAnimator.swift
@@ -0,0 +1,13 @@
+//
+//  TransitionAnimator.swift
+//  MovieDB - Nano Challenge
+//
+//  Created by Athos Lagemann on 07/06/17.
+//  Copyright © 2017 nano Challenge Movie DB. All rights reserved.
+//
+
+import UIKit
+
+class TransitionAnimator: NSObject {
+
+}
